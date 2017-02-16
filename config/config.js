168870d8e57ed0433f846e011eafe68f37b9c1d2
Matthieu Harbich <Matthieu@MBP-de-Matthieu.fritz.box>
2017-02-16 18:30:02 +0100

    config

---
+++ config/config.js
@@ -0,0 +1,69 @@
+/* Magic Mirror Config Sample
+ *
+ * By Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ */
+
+var config = {
+	port: 8080,
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],
+
+	language: 'en',
+	timeFormat: 24,
+	units: 'metric',
+
+	modules: [
+		{
+			module: 'alert',
+		},
+		{
+			module: "updatenotification",
+			position: "top_bar"
+		},
+		{
+			module: 'clock',
+			position: 'top_left'
+		},
+		{
+			module: 'compliments',
+			position: 'lower_third'
+		},
+		{
+			module: 'currentweather',
+			position: 'top_right',
+			config: {
+				location: 'Lausanne',
+				locationID: '2659994',  //ID from http://www.openweathermap.org
+				appid: '6dd28b9e1e402fb7f44319100b742f8f'
+			}
+		},
+		{
+			module: 'weatherforecast',
+			position: 'top_right',
+			header: 'Weather Forecast',
+			config: {
+				location: 'Lausanne',
+				locationID: '2659994',  //ID from http://www.openweathermap.org
+				appid: '6dd28b9e1e402fb7f44319100b742f8f'
+			}
+		},
+		{
+			module: 'newsfeed',
+			position: 'bottom_bar',
+			config: {
+				feeds: [
+					{
+						title: "New York Times",
+						url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true
+			}
+		},
+	]
+
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== 'undefined') {module.exports = config;}
